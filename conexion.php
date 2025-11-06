f1d3e4fc6bf648635f8172e23a5b26230aeab68d
alejandro-dab <alejandro_ab24@hotmail.com>
2025-11-06 00:14:26 -0600

    Optimización y función getenv para Render

--- conexion.php
+++ conexion.php
@@ -5,7 +5,16 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 //Variable conexion enviar 4 argumentos a la función php mysqli_connect
-$conexion = mysqli_connect("localhost", "root", "root", "Users");
+// Render y cualquier host profesional pasan las claves por el entorno
+/* La función  en PHP se utiliza para obtener el valor de una variable de entorno del sistema o del entorno de ejecución del servidor. 
+Es clave para acceder a configuraciones sensibles como claves API, credenciales o rutas sin exponerlas directamente en el código.
+*/
+$DB_HOST = getenv('DB_HOST') ?: "localhost"; 
+$DB_USER = getenv('DB_USER') ?: "root"; 
+$DB_PASS = getenv('DB_PASS') ?: "root"; 
+$DB_NAME = getenv('DB_NAME') ?: "Users"; 
+
+$conexion = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
 //Si la variable conexion es falso (si la conexión falló)
 if(!$conexion)
