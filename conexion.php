d96440e8ba2337b2711a560b48a08ea9b9ca8b58
alejandro-dab <alejandro_ab24@hotmail.com>
2025-11-06 07:14:48 -0600

    Conexion.php modificado para Railway y localhost

--- conexion.php
+++ conexion.php
@@ -5,17 +5,13 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 // Railway nos da estas variables.
 // Leemos las variables de entorno que vamos a crear en el panel de Railway
-$DB_HOST = getenv('DB_HOST');
-$DB_USER = getenv('DB_USER');
-$DB_PASS = getenv('DB_PASS');
-$DB_NAME = getenv('DB_NAME');
-$DB_PORT = getenv('DB_PORT'); // Railway necesita el puerto
+$host = getenv('MySQL.MYSQLHOST') ?: 'localhost';
+$user = getenv('MySQL.MYSQLUSER') ?: 'root';
+$pass = getenv('MySQL.MYSQLPASSWORD') ?: 'root';
+$db   = getenv('MySQL.MYSQLDATABASE') ?: 'Users';
 
-// Convertir puerto a entero
-$DB_PORT_INT = (int)$DB_PORT;
+$conexion = mysqli_connect($host, $user, $pass, $db);
 
-// Usamos las 5 variables para conectar
-$conexion = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT_INT);
 
 //Si la variable conexion es falso (si la conexión falló)
 if (!$conexion) {
